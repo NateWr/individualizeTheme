87adfc7adfd2d52a311241e0524631d84bb133fa
Nate Wright <natew@notthisway.com>
2024-09-30 17:12:02 +0100

    Add Vite integration

--- SlubTheme.inc.php
+++ SlubTheme.inc.php
@@ -1,45 +1,56 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+use NateWr\vite\Loader;
+
 import('lib.pkp.classes.plugins.ThemePlugin');
 
-class SlubTheme extends ThemePlugin {
+class SlubTheme extends ThemePlugin
+{
 
-	public function isActive() {
-		if (defined('SESSION_DISABLE_INIT')) return true;
-		return parent::isActive();
-	}
+    public function isActive()
+    {
+        if (defined('SESSION_DISABLE_INIT')) return true;
+        return parent::isActive();
+    }
 
-	public function init() {
-		$this->addOption('header', 'FieldOptions', [
-			'type' => 'radio',
-			'label' => __('plugins.themes.slubTheme.option.header.label'),
-			'description' => __('plugins.themes.slubTheme.option.header.description'),
-			'options' => [
-				[
-					'value' => 'default',
-					'label' => __('plugins.themes.slubTheme.option.header.default'),
-				],
-				[
-					'value' => 'defaultCenter',
-					'label' => __('plugins.themes.slubTheme.option.header.default-center'),
-				],
-				[
-					'value' => 'oneLine',
-					'label' => __('plugins.themes.slubTheme.option.header.oneLine'),
-				],
-			],
-			'default' => 'default',
-		]);
+    public function init()
+    {
+        $this->addOption('header', 'FieldOptions', [
+            'type' => 'radio',
+            'label' => __('plugins.themes.slubTheme.option.header.label'),
+            'description' => __('plugins.themes.slubTheme.option.header.description'),
+            'options' => [
+                [
+                    'value' => 'default',
+                    'label' => __('plugins.themes.slubTheme.option.header.default'),
+                ],
+                [
+                    'value' => 'defaultCenter',
+                    'label' => __('plugins.themes.slubTheme.option.header.default-center'),
+                ],
+                [
+                    'value' => 'oneLine',
+                    'label' => __('plugins.themes.slubTheme.option.header.oneLine'),
+                ],
+            ],
+            'default' => 'default',
+        ]);
 
-		$this->addMenuArea(array('primary', 'user'));
-	}
+        $this->addMenuArea(array('primary', 'user'));
 
-	public function getDisplayName() {
-		return __('plugins.themes.slubTheme.name');
-	}
+        $this->addViteAssets(['src/main.js']);
+    }
 
-	public function getDescription() {
-		return __('plugins.themes.slubTheme.description');
-	}
+    public function getDisplayName()
+    {
+        return __('plugins.themes.slubTheme.name');
+    }
+
+    public function getDescription()
+    {
+        return __('plugins.themes.slubTheme.description');
+    }
 
     /**
      * Get the URL to the theme's root directory
@@ -50,5 +61,47 @@ class SlubTheme extends ThemePlugin {
         $baseUrl = rtrim($request->getBaseUrl(), '/');
         $pluginPath = rtrim($this->getPluginPath(), '/');
         return "{$baseUrl}/{$pluginPath}";
+    }
+
+    /**
+     * Add the script, style and other assets compiled by Vite
+     */
+    protected function addViteAssets(array $entryPoints): void
+    {
+        $viteServerUrl = $this->getViteServerUrl();
+        $basePath = $viteServerUrl ? $viteServerUrl : join('/', [$this->getPluginUrl(), 'dist/']);
+
+        $viteLoader = new Loader(
+            templateManager: TemplateManager::getManager(Application::get()->getRequest()),
+            manifestPath: dirname(__FILE__) . '/dist/.vite/manifest.json',
+            basePath: $basePath,
+            devMode: $viteServerUrl ? true : false
+        );
+
+        $viteLoader->load($entryPoints);
+    }
+
+    /**
+     * Get the URL to the Vite server
+     *
+     * This checks the theme's root directory for a .vite.server.json
+     * file. If found, it reads the Vite server URL from that file.
+     *
+     * @see ./vite.config.js
+     */
+    protected function getViteServerUrl(): string
+    {
+        $path = join('/', [dirname(__FILE__), '.vite.server.json']);
+        if (!file_exists($path)) {
+            return '';
+        }
+        $config = json_decode(file_get_contents($path), true);
+        if (!$config) {
+            return '';
+        }
+        if (empty($config['network'])) {
+            return isset($config['local']) ? $config['local'][0] : 'http://localhost:5173/';
+        }
+        return $config['network'][0];
     }
 }
---
+++ readme.md
@@ -0,0 +1,31 @@
+# OJS Theme by SLUB Dresden
+
+...
+
+# Install
+
+...
+
+# Contribute
+
+This theme uses [Vite](https://vitejs.dev/) to build CSS/JS assets. Run the following commands to sync CSS/JS assets with Vite's HMR server while editing the theme.
+
+Install dependencies.
+
+```bash
+composer install
+npm install
+```
+
+Start Vite's development server.
+
+```bash
+npm run start
+```
+
+Build the assets for distribution.
+
+```bash
+npm run build
+```
+
