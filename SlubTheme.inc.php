ae89e59dcdd16255cae014f872d1f3fe6d01f671
Nate Wright <natew@notthisway.com>
2024-10-16 14:17:17 +0100

    Add default font: Inter

--- SlubTheme.inc.php
+++ SlubTheme.inc.php
@@ -23,6 +23,7 @@ class SlubTheme extends ThemePlugin
         $this->registerTemplatePlugins();
         $this->optionsHelper = new SlubThemeOptions($this);
         $this->optionsHelper->addOptions();
+        $this->addFonts();
         $this->addStyle('variables', $this->optionsHelper->getCssVariablesString(), ['inline' => true]);
         $this->addMenuArea(['primary', 'user', 'homepage']);
         $this->addViteAssets(['src/main.js']);
@@ -100,15 +101,48 @@ class SlubTheme extends ThemePlugin
     }
 
     /**
-     * Whether or not the theme's header layout option matches
-     * one of the passed options.
-     *
-     * @param string[] $values
+     * Load the selected fonts
      */
-    public function isHeader(array $values): bool
+    protected function addFonts(): void
     {
-        $header = $this->getOption('header');
-        return in_array($header, $values);
+        $this->addStyle(
+            'st-font',
+            "
+@font-face {
+  font-family: 'Inter';
+  font-style: italic;
+  font-weight: 100 900;
+  font-display: swap;
+  src: url({$this->getPluginUrl()}/fonts/inter/latin-ext-italic.woff2) format('woff2');
+  unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+@font-face {
+  font-family: 'Inter';
+  font-style: italic;
+  font-weight: 100 900;
+  font-display: swap;
+  src: url({$this->getPluginUrl()}/fonts/inter/latin-italic.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 100 900;
+  font-display: swap;
+  src: url({$this->getPluginUrl()}/fonts/inter/latin-ext.woff2) format('woff2');
+  unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 100 900;
+  font-display: swap;
+  src: url({$this->getPluginUrl()}/fonts/inter/latin.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+            ",
+            ['inline' => true]
+        );
     }
 
     /**
