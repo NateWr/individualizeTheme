84017194108efa32128b33734f2a44321cd70407
Nate Wright <natew@notthisway.com>
2025-11-26 15:46:21 +0000

    Show hosted using OJS at the bottom of the footer if not overridden by child theme

--- IndividualizeTheme.inc.php
+++ IndividualizeTheme.inc.php
@@ -107,6 +107,10 @@ class IndividualizeTheme extends ThemePlugin
         $request = Application::get()->getRequest();
         $context = $request->getContext();
 
+        $templateMgr->assign([
+            'themeRootUrl' => $this->getPluginUrl(),
+        ]);
+
         if ($template === 'frontend/pages/indexJournal.tpl') {
             $this->optionsHelper->getHomepageBlocks();
         }
